7bae555f0ca1ae150e18efe301ad399b1767d305
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2012-11-10 14:33:55 +0800

    added stuff to gitignore

--- booklat/settings.py
+++ booklat/settings.py
@@ -13,23 +13,21 @@ MANAGERS = ADMINS
 
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}  
 
-"""
 # DEVELOPMENT DATABASE DETAILS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'booklat',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': 'postgres',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/Users/fadhil/Documents/magic/django/booklat/sqlite3.db',                      # Or path to database file if using sqlite3.
+        #'USER': 'postgres',                      # Not used with sqlite3.
+#g       'PASSWORD': 'postgres',                  # Not used with sqlite3.
+#g       'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#g       'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
-"""
 
 # PRODUCTION DATABASE DETAILS
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -40,7 +38,7 @@ DATABASES = {
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
-
+'''
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
